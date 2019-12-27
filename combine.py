b2332af5413f7ec2d4b0b9c69f7493079b30ed57
tth9955 <59172409+tth9955@users.noreply.github.com>
2019-12-27 20:48:24 +0800

    Update combine.py

--- combine.py
+++ combine.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from bs4 import BeautifulSoup
 from selenium.webdriver.common.action_chains import ActionChains
 import win32clipboard
 import win32con
